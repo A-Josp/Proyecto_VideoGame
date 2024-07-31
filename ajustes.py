54c43c0f9c992bfd5dafeb241569242130cad8d1
A-Josp <amaya7anthony@gmail.com>
2024-07-30 23:32:33 -0600

    Add files via upload

--- ajustes.py
+++ ajustes.py
@@ -1,17 +1,12 @@
 class Ajustes:
     def __init__(self):
-        # Ajustes de pantalla
         self.anchura = 950
         self.altura = 725
-        self.fondo = 'imagenes/Fgp.jpg'
-
-        # Ajustes del coche
-        self.velocidadCoche = 2 
-
-        # Ajustes de las balas
-        self.colorBala = (60, 60, 60)
+        self.colorBala = (255, 0, 0)
         self.anchuraBala = 3
         self.alturaBala = 15
-        self.velocidadBala = 10
+        self.velocidadBala = 4
+        self.fondo = 'imagenes/Fgp.jpg'
+        
+        self.velocidadEnemigo = 1.6
 
-        self.velocidadEnemigo = 2
